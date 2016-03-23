6d660185761eaa77b630410324f3557dde448e63
Filipe Mondaini <filipemondaini@gmail.com>
2016-03-23 00:09:01 -0300

    Arrumando template e estado dos relés

--- app.py
+++ app.py
@@ -4,29 +4,30 @@ app = Flask(__name__)
 app.debug = False
 _serial = None
 channels = {
-    "ventilador\r\n": "desligado",
-    "carregador\r\n": "desligado",
-    "luz\r\n": "desligado",
-    "pc\r\n": "desligado",
+    "ventilador\r\n": "danger",
+    "carregador\r\n": "danger",
+    "luzes\r\n": "danger",
+    "pc\r\n": "danger",
 }
 
 
 def switch(name):
     if channels.has_key(name):
-        if channels[name] == "desligado":
-            channels[name] = "ligado"
+        if channels[name] == "danger":
+            channels[name] = "success"
         else:
-            channels[name] = "desligado"
+            channels[name] = "danger"
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
     action = None
     if request.method == 'POST':
         if request.form['action']:
-            action = "{action}\r\n".format(action=request.form['action'])
+            action = "{action}".format(action=request.form['action'])
             _serial.write(action.encode('ascii'))
             switch(action.lower());
 
+    print channels
     return render_template('index.html', action=action, channels=channels)
 
 if __name__ == "__main__":
