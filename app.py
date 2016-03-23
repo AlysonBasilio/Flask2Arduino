48656615a7c172fd99f2df6ddde8198c3471ff47
Filipe Mondaini <filipemondaini@gmail.com>
2016-03-22 22:52:12 -0300

    demonstracao do relay acionado na pagina

--- app.py
+++ app.py
@@ -1,10 +1,22 @@
 import serial
-from flask import Flask
-from flask import request
-from flask import render_template
+from flask import Flask, request, render_template
 app = Flask(__name__)
-app.debug = True
+app.debug = False
 _serial = None
+channels = {
+    "ventilador\r\n": "desligado",
+    "carregador\r\n": "desligado",
+    "luz\r\n": "desligado",
+    "pc\r\n": "desligado",
+}
+
+
+def switch(name):
+    if channels.has_key(name):
+        if channels[name] == "desligado":
+            channels[name] = "ligado"
+        else:
+            channels[name] = "desligado"
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
@@ -13,9 +25,10 @@ def index():
         if request.form['action']:
             action = "{action}\r\n".format(action=request.form['action'])
             _serial.write(action.encode('ascii'))
+            switch(action.lower());
 
-    return render_template('index.html', action=action)
+    return render_template('index.html', action=action, channels=channels)
 
 if __name__ == "__main__":
-    _serial = serial.Serial('com3', 9600, timeout=0)
+    _serial = serial.Serial('com5', 9600)
     app.run()
